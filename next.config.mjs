24df8a3c7a25b3edf54b45519ff1dda6ca7de9b0
Abdul Rahman <abdulr6503@gmail.com>
2025-02-19 13:30:07 +0530

    env updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      RESEND_API_KEY: process.env.RESEND_API_KEY,
+    },
+  };
+  
+  export default nextConfig;
+  
