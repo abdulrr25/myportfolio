2e347c96659fa07bc62ba7c1363f1c6874d283ec
Abdul Rahman <100862554+abdulrr25@users.noreply.github.com>
2025-11-27 16:30:00 +0530

    Update intro.tsx

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -104,7 +104,7 @@ I'm Abdul Rahman, a passionate full-stack developer dedicated to building modern
 
   {/* Resume Button */}
   <a
-    href="https://drive.google.com/file/d/15tz8kBrX_cI9s6snFUnA-Xhvns7v0Bvs/view?usp=sharing"
+    href="https://drive.google.com/file/d/1Rtz7E_Gmwgb8MMqdmaAb7QAjSe2VOsXQ/view?usp=drivesdk"
     target="_blank"
     rel="noopener noreferrer"
     className="bg-gray-900 p-4 text-white flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
